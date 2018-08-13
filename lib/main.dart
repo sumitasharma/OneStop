c0dfed2635ab5fb158400bee95f584f614b91c17
sumitasharma <30515910+sumitasharma@users.noreply.github.com>
2018-08-12 21:16:18 -0700

    Changed the name of the title.

--- lib/main.dart
+++ lib/main.dart
@@ -63,7 +63,7 @@ class RandomWordsState extends State<RandomWords> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Startup Name Generator'),
+        title: Text('Startup Name Generator First'),
       ),
       body: _buildSuggestions(),
     );
