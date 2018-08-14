1ec5634a1c8dc2cee3c1d6198d70b5364506cdf8
sumitasharma <30515910+sumitasharma@users.noreply.github.com>
2018-08-14 15:37:45 -0700

    OnTap added

--- lib/main.dart
+++ lib/main.dart
@@ -62,6 +62,15 @@ class RandomWordsState extends State<RandomWords> {
         alreadySaved ? Icons.favorite : Icons.favorite_border,
         color: alreadySaved ? Colors.red : null,
       ), // ... to here.
+      onTap: () { // Add 9 lines from here...
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      }, // ... to here.
     );
   }
 
