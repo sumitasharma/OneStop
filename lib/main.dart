5390c702b8ed8cc448f13cfa2dad4ae3caf76c07
sumitasharma <30515910+sumitasharma@users.noreply.github.com>
2018-08-22 22:18:09 -0700

    Try some visual changes.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,25 @@
 import 'package:flutter/material.dart';
 
-Widget newsRectangle() {
-  return Container(
-    color: Colors.greenAccent
-    ,);
+class newsRectangle extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child:
+        Container(
+          color: Colors.greenAccent,
+          height: 400.0,
+          width: 500.0,
+          child: Center(
+            child: Text(
+              'Daily News',
+              style: TextStyle(fontSize: 40.0),
+            ),
+          )
+          ,)
+    );
+  }
+
+
 }
 
 void main() {
