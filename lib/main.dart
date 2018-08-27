b62aad1b5602efe4b633f0bc0703017a5747456b
sumitasharma <30515910+sumitasharma@users.noreply.github.com>
2018-08-26 21:40:08 -0700

    Added Inkwell.

--- lib/main.dart
+++ lib/main.dart
@@ -3,19 +3,30 @@ import 'package:flutter/material.dart';
 class newsRectangle extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child:
-        Container(
-          color: Colors.greenAccent,
-          height: 400.0,
-          width: 500.0,
-          child: Center(
-            child: Text(
-              'Daily News',
-              style: TextStyle(fontSize: 40.0),
-            ),
-          )
-          ,)
+    return Material(
+        color: Colors.transparent,
+        child: Container(
+            child: Center(
+                child: InkWell(
+                    highlightColor: Color(50),
+                    splashColor: Color(100),
+                    onTap: () {
+                      print("Tapped");
+                    },
+                    child: Container(
+                      color: Colors.greenAccent,
+                      height: 400.0,
+                      width: 500.0,
+                      child: Center(
+                        child: Text(
+                          'Daily News',
+                          style: TextStyle(fontSize: 40.0),
+                        ),
+                      )
+                      ,)
+                )
+            )
+        )
     );
   }
 
