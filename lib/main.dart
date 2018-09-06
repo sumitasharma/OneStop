ed13df0c1e8df9cf8c823b6eacfea0620c71a74e
sumitasharma <30515910+sumitasharma@users.noreply.github.com>
2018-09-05 20:36:06 -0700

    ListViews.

--- lib/main.dart
+++ lib/main.dart
@@ -1,57 +1,35 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(new MaterialApp(
-      home: MyStatelessWidget()
-  ));
-}
+void main() => runApp(MyApp());
 
-class MyStatelessWidget extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(title: new Text("My AppBar")),
-      body: new Container(
-          padding: EdgeInsets.all(8.0),
-          child: new Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              new MyCardView(
-                  text: new Text("hello", style: new TextStyle(
-                      fontSize: 25.0, fontStyle: FontStyle.italic)),
-                  icon: new Icon(
-                      Icons.face, size: 40.0, color: Colors.limeAccent)
-              ),
-              new MyCardView(
-                  text: new Text("Bye", style: new TextStyle(
-                      fontSize: 25.0, fontStyle: FontStyle.italic)),
-                  icon: new Icon(Icons.accessibility_new, size: 40.0,
-                      color: Colors.lightGreen)
-              )
-            ],
-          )
+    final title = 'Global News';
+
+    return MaterialApp(
+      title: title,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: ListView(
+          children: <Widget>[
+            ListTile(
+              leading: Icon(Icons.map),
+              title: Text('Map'),
+            ),
+            ListTile(
+              leading: Icon(Icons.photo_album),
+              title: Text('Album'),
+            ),
+            ListTile(
+              leading: Icon(Icons.phone),
+              title: Text('Phone'),
+            ),
+          ],
+        ),
       ),
-    );
-  }
-}
-
-class MyCardView extends StatelessWidget {
-  MyCardView({this.text, this.icon});
-
-  final Widget text;
-  final Widget icon;
-
-  @override
-  Widget build(BuildContext context) {
-    return new Container(
-        child: new Card(
-          child: new Column(
-            children: <Widget>[
-              text,
-              icon
-            ],
-          ),
-        )
     );
   }
 }
