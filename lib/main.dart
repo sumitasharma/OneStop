305d084d4a2d673cdc3d109bf896024c20eae5fd
sumitasharma <30515910+sumitasharma@users.noreply.github.com>
2018-09-08 10:38:10 -0700

    Initial news page displayed,launch browser added.

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:convert' show json;
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(new MaterialApp(
@@ -32,6 +33,7 @@ class HomePageState extends State<HomePage> {
       print(name);
       data = decoded['articles'];
     });
+    print(data[1]["urlToImage"]);
     return "Success!";
   }
 
@@ -47,15 +49,68 @@ class HomePageState extends State<HomePage> {
       appBar: new AppBar(
         title: new Text("Listviews"),
       ),
-      body: new ListView.builder(
+        body: new Container(child: new ListView.builder(
         itemCount: data == null ? 0 : data.length,
         itemBuilder: (BuildContext context, int index) {
           return new Card(
-            child: new Text(data[index]["title"]),
+              child: new InkWell(
+                  onTap: () =>
+                  //_launchURL(data[index]["url"]),
+                  Navigator.of(context).push(new MaterialPageRoute(
+                      builder: (BuildContext context) => new SecondScreen(
+                          data[index]["url"]))),
+                  child: new Column(
+                    children: <Widget>[
+                      data[index]["source"]["name"] == null
+                          ? new Text("")
+                          : new Text(data[index]["source"]["name"]),
+                      data[index]["title"] == null
+                          ? new Text("Missing Title")
+                          : new Text(data[index]["title"]),
+                      data[index]["urlToImage"] == null
+                          ? new Image.network(
+                          " https://static01.nyt.com/images/2018/09/08/business/08dc-trade/08dc-trade-facebookJumbo.jpg")
+                          : new Image.network(data[index]["urlToImage"]),
+                      data[index]["description"] == null
+                          ? new Text("")
+                          : new Text(data[index]["description"]),
+                      // new FlatButton(onPressed: null, child: null)
+
+                    ],
+                  )
+              )
           );
         },
-      ),
+        )
+        )
     );
   }
+}
+
+
+class SecondScreen extends StatelessWidget {
+  final String _url;
+
+  SecondScreen([this._url]);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Second Screen"),
+        ),
+        body: _launchURL(_url)
+    );
+  }
+
+  _launchURL(url) async {
+    //const url = url;
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
 }
 
