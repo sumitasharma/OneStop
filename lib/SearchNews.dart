9e8d0d78e3ed6ee2e3d13dba64a57cf4f3eaf330
sumitasharma <30515910+sumitasharma@users.noreply.github.com>
2018-11-17 08:00:16 -0800

    Removing redundant comments.

--- lib/SearchNews.dart
+++ lib/SearchNews.dart
@@ -46,7 +46,7 @@ class SearchNewsState extends State<SearchNews> {
 
   bool isInternetConnectivity() {
     var connectivityResult = new Connectivity()
-        .checkConnectivity(); // User defined class
+        .checkConnectivity(); 
     if (connectivityResult == ConnectivityResult.mobile ||
         connectivityResult == ConnectivityResult.wifi) {
       return true;
